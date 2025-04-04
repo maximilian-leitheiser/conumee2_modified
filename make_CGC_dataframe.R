f6ed7f4585b62ca0b3dc2a9de6b79fa60ffa9327
maximilian-leitheiser <maximilian.leitheiser@charite.de>
2025-04-04 22:17:10 +0200

    include CGC_dataframe properly in package

---
+++ make_CGC_dataframe.R
@@ -0,0 +1,4 @@
+Cosmic_CancerGeneCensus_v99_GRCh37 = read.csv("/mnt/nas/private/leitheim/CNV_analysis/CGC_annotation/Cosmic_CancerGeneCensus_v99_GRCh37.tsv", sep = "\t")
+
+save(list = c("Cosmic_CancerGeneCensus_v99_GRCh37"), 
+     file = "/mnt/nas/private/leitheim/CNV_analysis/conumee2_edits/conumee2_modified/data/Cosmic_CancerGeneCensus_v99_GRCh37.rda")
